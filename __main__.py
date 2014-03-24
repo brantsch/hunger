6eacd5ee3f3c418722256c5998e2c73f8c7a1173
Peter Brantsch <peter.brantsch@kabelmail.de>
2014-03-24 18:29:40 +0100

    added --date switch

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import sys
 from mensa.parser import parse
 from mensa.util import fetch
 import argparse
@@ -7,7 +8,6 @@ import pickle
 from datetime import date
 
 description = """
-List contents of canteen menu of the THI mensa.
 """
 
 dishes = None
@@ -41,32 +41,51 @@ def list_all():
 		print(dish)
 
 def list_today():
-	today = date.today()
+	list_for_date(date.today())
+
+def list_for_date(thedate):
+	weekday = thedate.weekday()
+	if weekday >= 5:
+		print("Error: given date is on a weekend. Please try {0} or {1} instead.".format(\
+			date.fromordinal(thedate.toordinal()+(7-weekday)),\
+			date.fromordinal(thedate.toordinal()-(weekday-4))\
+		),file=sys.stderr)
+		exit(1)
 	global dishes
 	need_update = True
 	if dishes: 
 		for dish in dishes:
-			if dish.date == today:
+			if dish.date == thedate:
 				need_update = False
 				print(dish)
 	if need_update:
-		print("No entry found for today. Will now update cache. Try again after cache is updated.")
+		print("No entry found for date {0}. Will now update cache. Try again after cache is updated.".format(thedate))
 		update()
-
+	
 def main():
-	parser = argparse.ArgumentParser(description)
-	parser.add_argument('--force-update','-u',action='store_true')
-	parser.add_argument('--drop-cache',action='store_true')
-	parser.add_argument('--list-all',action='store_true')
+	parser = argparse.ArgumentParser("List contents of canteen menu of the THI mensa.")
+	parser.add_argument('--force-update','-u',action='store_true',help="Force update of the cache and exit.")
+	parser.add_argument('--drop-cache',action='store_true',help="Delete cache file.")
+	parser.add_argument('--list-all',action='store_true',help="List all available dishes.")
+	parser.add_argument('--date','-d',type=str,help="Show menu for date given as YYYY-MM-DD")
 	args = parser.parse_args()
 	if args.drop_cache:
 		drop_cache()
 		exit()
 	if args.force_update:
 		update()
+		exit()
 	load()
 	if args.list_all:
 		list_all()
+	elif args.date:
+		year, month, day = map(lambda x: int(x),args.date.split('-'))
+		try:
+			d = date(year, month, day)
+		except ValueError:	
+			print("Invalid date!",file=sys.stderr)
+			exit(1)
+		list_for_date(d)
 	else:
 		list_today()
 	
